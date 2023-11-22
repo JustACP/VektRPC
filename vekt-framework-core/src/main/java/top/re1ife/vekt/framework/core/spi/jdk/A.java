f4dd57e934e16a00d7622c759f794a9df00a1f63
re1ife <justacp@163.com>
2023-11-22 18:58:13 +0800

    doc: README add

---
+++ vekt-framework-core/src/main/java/top/re1ife/vekt/framework/core/spi/jdk/A.java
@@ -0,0 +1,14 @@
+package top.re1ife.vekt.framework.core.spi.jdk;
+
+/**
+ * @author re1ife
+ * @description:
+ * @date 2023/08/21 21:59:10
+ * @Copyright：re1ife | blog: re1ife.top
+ */
+public class A implements ISpiTest{
+    @Override
+    public void doSomething() {
+        System.out.println("vvvv");
+    }
+}
